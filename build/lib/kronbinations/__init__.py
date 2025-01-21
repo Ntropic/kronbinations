84ffaf3dbcb317f5ec7f21cd73a09520f752e20d
Ntropic <21343359+Ntropic@users.noreply.github.com>
2025-01-22 00:22:16 +0100

    changed pathos requirement and bumped version

--- build/lib/kronbinations/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from kronbinations.kronbinations import *
-from kronbinations.JIT_kronbinations import *
-from kronbinations.JIT_kronbinations_load import *
-from kronbinations.JIT_Array import *
-from kronbinations.Kron_Array import *
-from kronbinations.Kron_Fun_Modifier import *
